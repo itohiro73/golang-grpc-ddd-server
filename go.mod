06fbb28a433116e3e21ee10f777ec75efdf54b67
keitakn <keita.koga.work@gmail.com>
2020-01-09 16:16:44 +0900

    :art: #2 main.goで簡単なgRPCサーバーが起動するように変更

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/keitakn/golang-grpc-server
 
 go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	google.golang.org/grpc v1.26.0
+)
---
+++ infrastructure/gRPCService.go
@@ -0,0 +1,28 @@
+package infrastructure
+
+import (
+	"context"
+	"errors"
+	pb "github.com/keitakn/golang-grpc-server/pb"
+)
+
+type CatService struct{}
+
+func (s *CatService) FindMyCat(ctx context.Context, message *pb.FindCuteCatMessage) (*pb.CuteCatResponse, error) {
+	switch message.CatId {
+	case "moko":
+		// もこはチンチラシルバー
+		return &pb.CuteCatResponse{
+			Name: "Moko",
+			Kind: "Chinchilla silver",
+		}, nil
+	case "mop":
+		// もっぷはマンチカン
+		return &pb.CuteCatResponse{
+			Name: "Mop",
+			Kind: "Munchkin",
+		}, nil
+	}
+
+	return nil, errors.New("Not Found Cat")
+}
