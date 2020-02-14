676634da4d1d10bf386005ab7d8b88f34fdf50b8
keitakn <keita.koga.work@gmail.com>
2020-02-14 17:30:34 +0900

    :recycle: #9 認可処理をより具体的な実装にする為にBearerトークンをJWT形式で受け取るように変更

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/keitakn/golang-grpc-server
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.3.2
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
 	google.golang.org/grpc v1.26.0
--- infrastructure/authentication.go
+++ infrastructure/authentication.go
@@ -2,6 +2,7 @@ package infrastructure
 
 import (
 	"context"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -20,14 +21,26 @@ func Authentication(ctx context.Context) (context.Context, error) {
 		)
 	}
 
-	return setAuthToken(ctx, token), nil
+	// サンプルなので署名検証はスキップ
+	// payloadにsubが含まれていれば問題なし
+	parser := new(jwt.Parser)
+	parsedToken, _, err := parser.ParseUnverified(token, &jwt.StandardClaims{})
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Unauthenticated,
+			"could not parsed auth token: %v",
+			err,
+		)
+	}
+
+	return setAuthToken(ctx, parsedToken.Claims.(*jwt.StandardClaims)), nil
 }
 
-func setAuthToken(ctx context.Context, token string) context.Context {
+func setAuthToken(ctx context.Context, token *jwt.StandardClaims) context.Context {
 	return context.WithValue(ctx, AuthTokenKey, token)
 }
 
-func GetAuthToken(ctx context.Context) (string, bool) {
-	val, ok := ctx.Value(AuthTokenKey).(string)
+func GetAuthToken(ctx context.Context) (*jwt.StandardClaims, bool) {
+	val, ok := ctx.Value(AuthTokenKey).(*jwt.StandardClaims)
 	return val, ok
 }
