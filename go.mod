9539fb9ee3e3e248a2c7b684509da9fe9008933a
keitakn <keita.koga.work@gmail.com>
2020-02-18 13:48:59 +0900

    :sparkles: #15 公式サンプルを参考にgRPCのヘルスチェックメソッドを実装

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.3.2
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
+	github.com/sirupsen/logrus v1.4.2
 	go.uber.org/zap v1.10.0
-	google.golang.org/grpc v1.26.0
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55
+	google.golang.org/grpc v1.27.0
 )
---
+++ infrastructure/gRPCHealthCheck.go
@@ -0,0 +1,31 @@
+package infrastructure
+
+import (
+	"context"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	health "github.com/keitakn/golang-grpc-server/google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type SkipAuthHealthServer struct {
+	health.HealthServer
+}
+
+var _ grpc_auth.ServiceAuthFuncOverride = (*SkipAuthHealthServer)(nil)
+
+// AuthFuncOverride SkipAuthHealthServer構造体がServiceAuthFuncOverrideインターフェースを実装する
+func (*SkipAuthHealthServer) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
+	return ctx, nil
+}
+
+func (h *SkipAuthHealthServer) Check(context.Context, *health.HealthCheckRequest) (*health.HealthCheckResponse, error) {
+	// ここまで処理が来ればヘルスチェックとしては成功となる
+	return &health.HealthCheckResponse{
+		Status: health.HealthCheckResponse_SERVING,
+	}, nil
+}
+
+func (h *SkipAuthHealthServer) Watch(*health.HealthCheckRequest, health.Health_WatchServer) error {
+	return status.Error(codes.Unimplemented, "service watch is not implemented current version.")
+}
